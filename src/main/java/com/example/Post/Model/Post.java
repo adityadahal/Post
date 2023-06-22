96452f8d11e5e2c54f0830140591e85522843133
Aditya Dahal <toadityadahal@gmail.com>
2023-06-22 14:06:26 +0545

    Model for Comment added

--- src/main/java/com/example/Post/Model/Post.java
+++ src/main/java/com/example/Post/Model/Post.java
@@ -1,9 +1,6 @@
 package com.example.Post.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +17,10 @@ public class Post {
     private  String title;
 
     private String description;
+
+    @OneToMany
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
 
 
 }

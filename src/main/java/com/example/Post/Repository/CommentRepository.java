a9d941c9eb757c108ef75bab1219c12887e8f483
Aditya Dahal <toadityadahal@gmail.com>
2023-06-23 13:29:46 +0545

    Comment Repo Find by Post id

--- src/main/java/com/example/Post/Repository/CommentRepository.java
+++ src/main/java/com/example/Post/Repository/CommentRepository.java
@@ -4,7 +4,9 @@ import com.example.Post.Model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findAllByPostId(int id);
 }

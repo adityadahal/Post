40f60d8ef54f08ff788da1278a6c00a7bf7ad7f5
Aditya Dahal <toadityadahal@gmail.com>
2023-06-23 13:28:57 +0545

    Dto for comment

--- src/main/java/com/example/Post/Dto/CommentDto.java
+++ src/main/java/com/example/Post/Dto/CommentDto.java
@@ -1,9 +1,11 @@
 package com.example.Post.Dto;
 
+import com.example.Post.Model.Post;
 import lombok.Data;
 
 @Data
 public class CommentDto {
-    private  int id;
-    private  String comment;
+    private int id;
+    private String comment;
+    private int postId;
 }

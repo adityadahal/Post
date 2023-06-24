814d94117807d12c6dcf22dc93a4f5a93d181fee
Aditya Dahal <toadityadahal@gmail.com>
2023-06-24 15:30:10 +0545

    bug fix: Repository Annotion added in Like

--- src/main/java/com/example/Post/Repository/LikeRepository.java
+++ src/main/java/com/example/Post/Repository/LikeRepository.java
@@ -2,7 +2,9 @@ package com.example.Post.Repository;
 
 import com.example.Post.Model.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LikeRepository extends JpaRepository<Like, Integer> {
     Like findAllByPostId(int id);
 }

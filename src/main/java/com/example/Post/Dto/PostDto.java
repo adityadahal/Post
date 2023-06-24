750beba0a47b54f066a40d0d7890dc9db416d9ad
Aditya Dahal <toadityadahal@gmail.com>
2023-06-24 15:28:08 +0545

    Dto For Post

--- src/main/java/com/example/Post/Dto/PostDto.java
+++ src/main/java/com/example/Post/Dto/PostDto.java
@@ -1,6 +1,7 @@
 package com.example.Post.Dto;
 
 import com.example.Post.Model.Comment;
+import com.example.Post.Model.Like;
 import lombok.Data;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public class PostDto {
     private String title;
     private String description;
     private List<Comment> comments;
+    private Like like;
 }
